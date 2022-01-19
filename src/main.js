316f1822d77ccba457a19cb3afba09f263b976bd
LzhBryan <86075657+LzhBryan@users.noreply.github.com>
2022-01-20 00:12:55 +0800

    Initialized electron react project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -12,11 +12,6 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      preload: path.join(__dirname, "preload.js")
-    }
   });
 
   // and load the index.html of the app.
@@ -41,7 +36,12 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.

36f804b508bbfd6c74b7582d7aceababb85c0514
wyshbro <93894053+wyshbro@users.noreply.github.com>
2022-02-24 10:20:37 +0800

    Refactoring for login and registration

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,31 +1,31 @@
 const User = require("../models/user")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
 
 const registration = async (req, res) => {
-  const HASHED_PASSWORD = await bcrypt.hash(req.body.password, 10)
-
-  const user = await User.create({
-    username: req.body.username,
-    password: HASHED_PASSWORD,
-  })
-  res.status(201).json({ user })
+  const user = await UserModel.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(201).json({ name: user.username, token })
 }
 
 const login = async (req, res) => {
-  const user = await User.findOne({ username: req.body.username }).lean()
+  const { username, password } = req.body
 
-  if (bcrypt.compare(req.body.password, user.password)) {
-    const token = jwt.sign(
-      {
-        id: user._id,
-        username: user.username,
-      },
-      process.env.JWT_SECRET
-    )
-    console.log("Success: ", token)
-    res.status(200).json({ user })
+  if (!username || !password) {
+    res.status(400).json({ msg: "Please provide email or password" })
   }
+
+  const user = await UserModel.findOne({ username: req.body.username })
+
+  if (!user) {
+    return res.status(401).json({ msg: "Invalid" })
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    return res.status(401).json({ msg: "Invalid" })
+  }
+
+  const token = user.createJWT()
+  res.status(200).json({ name: user.username, token })
 }
 
 module.exports = {
